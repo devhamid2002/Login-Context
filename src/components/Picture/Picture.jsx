e0fda25b25bdf1c1c1dc90af6e186014da745615
Hamidreza Yavarzadeh <hamid.yavarzadeh2002@gmail.com>
2023-11-16 14:15:04 +0330

    feat : add Picture camponent

---
+++ src/components/Picture/Picture.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import valo from "../assets/images/valorant.jpg";
+
+function Picture() {
+  return (
+    <>
+      <img src={valo} alt="" />
+    </>
+  );
+}
+
+export default Picture;
